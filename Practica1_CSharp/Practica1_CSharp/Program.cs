9087573e22a1a1cab30ffaf582fbc56a03c15471
RIvanCF <flores.ivan940214@gmail.com>
2023-02-25 19:31:34 -0600

    Primera practica de cSharp concluida - P1-Hola mundo

---
+++ Practica1_CSharp/Practica1_CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; //Esto es un paquete de comandos 
+
+namespace Practica1_CSharp
+{
+    class Program//Esto es una clase propia
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo al mundo de la programación en C#, soy Iván y estoy estudiando este lenguaje");
+        }
+    }
+}
